3e0cdb7ae987f79d54a6c05ed9da19479637f293
Sajtos Timudzsin <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2024-10-24 15:54:35 +0200

    if/else

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -3,7 +3,15 @@ package javagit;
 public class JavaGit {
     
     public static void main(String[] args) {
-        
+        int homerseklet = 7;
+        if(homerseklet < 10)
+        {
+            System.out.println("viszek kabátot");
+        }
+        else
+        {
+            System.out.println("nem viszek kabátot");
+        }
     }
     
 }
