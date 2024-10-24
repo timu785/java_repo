b811b9ce6b3b8421475d42f4d3d53c928bbe2ab7
Sajtos Timudzsin <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2024-10-24 16:02:31 +0200

    ternalis operator

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -4,14 +4,18 @@ public class JavaGit {
     
     public static void main(String[] args) {
         int homerseklet = 7;
+        String eredmeny = "nem viszek kabatot";
         if(homerseklet < 10)
         {
-            System.out.println("viszek kabátot");
+            eredmeny = "viszek kabátot";
         }
-        else
-        {
-            System.out.println("nem viszek kabátot");
-        }
+        System.out.println(eredmeny);
+        
+        final int HIDEG_HATAR = 10;
+        int fok = 7;
+        boolean hidegVan = fok < HIDEG_HATAR;
+        eredmeny = hidegVan ? "nem viszek" : "viszek";
+        System.out.println(eredmeny);
     }
     
 }
